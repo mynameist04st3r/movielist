3635fe69dc73df3da8a2a49c6c98251412f45a87
t04st3r@mac.com <t04st3r@mac.com>
2025-03-27 16:23:59 -0400

    Set up knexfile.js, created migrate file, built schema in migrate file, and ran both migrate:latest and migrate:rollback. Ended by running migrate:latest so the table is ready to go.

---
+++ server/knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: '127.0.0.1',
+      password: 'docker',
+      user: 'postgres',
+      port: 5432,
+      database: 'movies'
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      port: 5432,
+      database: 'movies',
+      user:     'postgres',
+      password: 'docker'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
 const http = require('http');
+
+const server = http.createServer((req, res) => {
+  let url = req.url.split('/');
+  if (url[1] === '' ) {
+    if (req.method === 'GET') {
+      res.end("Hello, World");
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+});
+
+server.listen(3000, "localhost", () => {
+  const addr = server.address();
+  console.log(`Server running at http://${addr.address}:${addr.port}`)
+});
