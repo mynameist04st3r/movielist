1c20738d6489c23d98217a8a312ee479a8b85e8a
t04st3r@mac.com <t04st3r@mac.com>
2025-03-30 17:16:20 -0400

    Fixed broken server issues.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-const port = 8080;
+const port = 8000;
 const express = require('express');
 const app = express();
-const knex = require('knex');
+const knex = require('knex')(require('./knexfile.js')['development']);
 
 
 app.get('/', (req, res) => {
   res.send('Here it is.');
 })
 app.get('/movies', (req, res) => {
-  knex('movies').select('*').then(movies => {
-    res.json(movies)
+  knex('movies').select('*').then(title => {
+    let movieTitles = title.map(movies => movies.title)
+    res.json(movieTitles);
   })
 });
 
